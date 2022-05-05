391064971c726a5508d2cf577206d6f4821ab2a8
Mark giliadov <markgiliadov@gmail.com>
2022-05-05 20:30:24 +0300

    deleting redundant code and files

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import { useEffect ,Component,Fragment} from "react";
+import { useEffect, Component, Fragment } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {Home}from './pages/Home';
-import {Login}from './pages/Login';
-import {SignUp}from './pages/SignUp';
-import {TeamUp}from './pages/TeamUp';
-import {Events}from './pages/Events';
-import {MyProfile}from './pages/MyProfile';
-import {Chat}from './pages/Chat';
-import {NoMatch}from './pages/NoMatch';
-import {NavigationBar}from './components/NavigationBar';
-import { fetchData, fetchUser,fetchInstance } from "./data/data";
+import { Home } from "./containers/Home";
+import { Login } from "./containers/Login";
+import { SignUp } from "./containers/SignUp";
+import { TeamUp } from "./containers/TeamUp";
+import { Events } from "./containers/Events";
+import { MyProfile } from "./containers/MyProfile";
+import { Chat } from "./containers/Chat";
+import { NoMatch } from "./containers/NoMatch";
+import { NavigationBar } from "./components/NavigationBar";
+import { fetchData, fetchUser, fetchInstance } from "./data/data";
 const App = () => {
   useEffect(() => {
     fetchUser().then((data) => console.log(data));
     fetchInstance().then((data) => console.log(data));
   }, []);
-  return(
-      //set
-      <BrowserRouter>
-        <NavigationBar/>
-          <Routes>
-            <Route exact path="/" element={<Home/>}/>
-            <Route exact path="/login" element={<Login/>}/>
-            <Route exact path="/signup" element={<SignUp/>}/>
-            <Route exact path="/findPartner" element={<TeamUp/>}/>
-            <Route exact path="/findEvent" element={<Events/>}/>
-            <Route exact path="/myProfile" element={<MyProfile/>}/>
-            <Route exact path="/chat" element={<Chat/>}/>
-            <Route element={<NoMatch/>}/>
-
-          </Routes>
-
-      
-      </BrowserRouter>
+  return (
+    //set
+    <BrowserRouter>
+      <NavigationBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/findPartner" element={<TeamUp />} />
+        <Route exact path="/findEvent" element={<Events />} />
+        <Route exact path="/myProfile" element={<MyProfile />} />
+        <Route exact path="/chat" element={<Chat />} />
+        <Route element={<NoMatch />} />
+      </Routes>
+    </BrowserRouter>
   );
- 
 };
 
 export default App;
