f21b457c32f20c1b25b5a1e9cfb93d4e44d8fc67
Mark giliadov <markgiliadov@gmail.com>
2022-05-05 21:16:28 +0300

    updated signup - to mongo connection worked

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col, Container, Form, Button } from "react-bootstrap";
 import { Footer } from "../components/Footer";
 import Img from "../assets/images/background-promo-event.jpg";
 import Logo from "../assets/images/logo2.png";
-function SignUp() {
+import { postUser } from "../data/data";
+const SignUp = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const user = {
+      username: firstName + "_" + lastName,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      role: "player",
+      avatar: "asd.jpg",
+    };
+    postUser(user);
+    console.log("user: " + firstName, lastName, password, email);
+  };
   const myStyle = {
     backgroundColor: "#DAAD86",
     position: "relative",
@@ -23,36 +41,60 @@ function SignUp() {
           <h1>RideAlong</h1>
           <hr></hr>
           <div className="color-overlay d-flex justify-content-center">
-            <Form className="rounded p-4 p-md-3 ">
-              <Row style={{paddingBottom:"10px"}}>
+            <Form onSubmit={handleSignUp} className="rounded p-4 p-md-3 ">
+              <Row style={{ paddingBottom: "10px" }}>
                 <Col>
-                  <Form.Control placeholder="First name" />
+                  <Form.Control
+                    placeholder="First name"
+                    onChange={(e) => {
+                      setFirstName(e.target.value);
+                    }}
+                  />
                 </Col>
                 <Col>
-                  <Form.Control placeholder="Last name" />
+                  <Form.Control
+                    placeholder="Last name"
+                    onChange={(e) => {
+                      setLastName(e.target.value);
+                    }}
+                  />
                 </Col>
               </Row>
               <Row>
                 <Col>
                   <Form.Group className="mb-2" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter Email" />
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter Email"
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                    />
                   </Form.Group>
                 </Col>
                 <Col>
                   <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="password" />
+                    <Form.Control
+                      type="password"
+                      placeholder="password"
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                    />
                   </Form.Group>
                 </Col>
               </Row>
+              <Button
+                type="submit"
+                className="rounded-5"
+                variant="outline-dark"
+                style={{ borderRadius: "500px", marginTop: "20px" }}
+              >
+                REGISTER
+              </Button>
             </Form>
           </div>
-          <Button
-            className="rounded-5"
-            variant="outline-dark"
-            style={{ borderRadius: "500px" }}
-          >
-            REGISTER
-          </Button>
+
           <br />
           <br />
           <hr style={{ width: "70%", display: "inline-block" }}></hr>
@@ -80,5 +122,5 @@ function SignUp() {
       <Footer />
     </>
   );
-}
+};
 export { SignUp };
--- src/data/data.js
+++ src/data/data.js
@@ -1,31 +1,41 @@
 const GET_USER_ENDPOINT =
-"http://localhost:8085/iob/users/login/2022b.yarden.dahan/admin@google.com";
+  "http://localhost:8085/iob/users/login/2022b.yarden.dahan/admin@google.com";
 const GET_INSTANCE_ENDPOINT =
   "http://localhost:8085/iob/instances/search/byName/Yossi?userDomain=2022b.yarden.dahan&userEmail=manager@google.com";
-  export const fetchUser = async () => {
-    const data = await fetch(GET_USER_ENDPOINT, {
-      method: "GET",
-      mode: "cors",
-      headers: { 
-        
-        "Accept": "application/json",
-      },
-      
-    }).then((rawData) => rawData.json());
-    return data;
-  };
-  export const fetchInstance = async () => {
-    
-    const response = await fetch(GET_INSTANCE_ENDPOINT, {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Accept": "application/json",
-      }
-      ,
+const POST_USER_ENDPOINT = "http://localhost:8085/iob/users?";
+export const fetchUser = async () => {
+  const data = await fetch(GET_USER_ENDPOINT, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      Accept: "application/json",
+    },
+  }).then((rawData) => rawData.json());
+  return data;
+};
+export const fetchInstance = async () => {
+  const response = await fetch(GET_INSTANCE_ENDPOINT, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      Accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log("Authorization failed: " + error.message));
+  return response;
+};
 
-    }).then(response => response.json())
-    .catch(error => console.log('Authorization failed: ' + error.message));
-    return response
-  };
+export const postUser = async (user) => {
+  console.log(JSON.stringify(user));
+  fetch(POST_USER_ENDPOINT, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+};
