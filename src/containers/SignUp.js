e66b6373a9f2f06edefe327a40f41c61150215c6
Yarden Dahan <68491359+Jordan-me@users.noreply.github.com>
2022-05-06 09:49:53 +0300

    add validation for signUp form

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -36,7 +36,6 @@ const SignUp = () => {
     console.log("user: " + firstName, lastName, password, email);
   };
 
-
   const handleSubmit = (event) => {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
@@ -66,7 +65,98 @@ const SignUp = () => {
           <h1>RideAlong</h1>
           <hr></hr>
           <div className="color-overlay d-flex justify-content-center">
-            <Form onSubmit={handleSignUp} className="rounded p-4 p-md-3 ">
+            <Form
+              noValidate
+              validated={validated}
+              onSubmit={handleSubmit}
+              className="rounded p-4 p-md-3 "
+            >
+              <Row className="mb-3" style={{ paddingBottom: "10px" }}>
+                <Form.Group as={Col} md="4" controlId="validationCustom01">
+                  <Form.Control required type="text" placeholder="First name" />
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="4" controlId="validationCustom02">
+                  <Form.Control required type="text" placeholder="Last name" />
+                </Form.Group>
+              </Row>
+              <Row className="mb-3">
+                <Form.Group as={Col} md="4" controlId="validationCustom02">
+                  <Form.Select
+                    className="form-select"
+                    id="validationCustom04"
+                    required
+                  >
+                    <option selected disabled value="">
+                      Select gender
+                    </option>
+                    <option value="1">Female</option>
+                    <option value="2">Man</option>
+                    <option value="3">Unknown</option>
+                  </Form.Select>
+                  <div className="invalid-feedback">Please select gender.</div>
+                </Form.Group>
+                <Form.Group as={Col} md="4" controlId="validationCustom02">
+                  <Form.Control required type="date" name="date_of_birth" />
+                </Form.Group>
+              </Row>
+              <Row className="mb-2">
+                <Form.Group
+                  className="mb-2"
+                  as={Row}
+                  md="4"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Control
+                    required
+                    type="email"
+                    placeholder="Enter Email"
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  />
+                </Form.Group>
+              </Row>
+              <Row className="mb-2">
+              <Form.Group className="mb-2" as={Row} controlId="formBasicPassword">
+                    <Form.Control
+                      required type="password"
+                      placeholder="password"
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                    />
+                  </Form.Group>
+              </Row>
+              <Row>
+                <Col>
+              <Form.Group className="mb-3" >
+                    <Form.Check
+                      required
+                      name="terms"
+                      label="Agree to terms and conditions"
+                      feedbackType="invalid"
+                      style={{ marginTop: "10px" }}
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Button
+                type="submit"
+                className="rounded-5"
+                variant="outline-dark"
+                style={{ borderRadius: "500px", marginTop: "20px" }}
+              >
+                REGISTER
+              </Button>
+            </Form>
+{/* 
+            <Form
+              noValidate
+              validated={validated}
+              onSubmit={handleSignUp}
+              className="rounded p-4 p-md-3 "
+            >
               <Row style={{ paddingBottom: "10px" }}>
                 <Col>
                   <Form.Control
@@ -111,14 +201,13 @@ const SignUp = () => {
               </Row>
               <Row>
                 <Col>
-                  <Form.Control 
-                    type="date" 
-                    name='date_of_birth' 
-                    
-                     />
+                  <Form.Control type="date" name="date_of_birth" />
                 </Col>
                 <Col xs={12} md={8}>
-                  <Form.Select aria-label="Default select example" style={{marginTop: "10px"}} >
+                  <Form.Select
+                    aria-label="Default select example"
+                    style={{ marginTop: "10px" }}
+                  >
                     <option>Select gender</option>
                     <option value="1">Female</option>
                     <option value="2">Man</option>
@@ -139,8 +228,8 @@ const SignUp = () => {
                   </Form.Group>
                 </Col>
                 <Col></Col>
-              </Row>
-              <Button
+              </Row> */}
+              {/* <Button
                 type="submit"
                 className="rounded-5"
                 variant="outline-dark"
@@ -148,7 +237,7 @@ const SignUp = () => {
               >
                 REGISTER
               </Button>
-            </Form>
+            </Form> */}
           </div>
 
           <br />
