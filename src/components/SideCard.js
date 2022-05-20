b656fd7f51cd78ac2904ee567cf00fdea162d383
Yarden Dahan <68491359+Jordan-me@users.noreply.github.com>
2022-05-20 19:13:24 +0300

    b

--- src/components/SideCard.js
+++ src/components/SideCard.js
@@ -15,7 +15,7 @@ function SideCard() {
 
   useEffect(() => {
     console.log(loggedInState);
-    if (loggedInState !== null ) {
+    if (loggedInState !== null) {
       setUser(loggedInState.user);
       setExtra(loggedInState.extra[0]);
     }
@@ -26,11 +26,9 @@ function SideCard() {
   return (
     <div>
       <div
-      
         className="offcanvas-body px-2 px-lg-0 d-none d-xl-block"
         style={{ backgroundColor: "#fff" }}
       >
-
         <div className="card overflow-hidden">
           <div className="card-body pt-0">
             <div className="text-center">
@@ -47,14 +45,14 @@ function SideCard() {
               </div>
               {/* info */}
               <h5 className="mb-0">
-              {loggedInState
-                    ? loggedInState.user
-                      ? loggedInState.user.username
-                      : null
-                    : null}
-                </h5>
-              <strong> 
-              {loggedInState ? (
+                {loggedInState
+                  ? loggedInState.user
+                    ? loggedInState.user.username
+                    : null
+                  : null}
+              </h5>
+              <strong>
+                {loggedInState ? (
                   loggedInState.extra[0] ? (
                     <p>
                       {loggedInState.extra[0].instanceAttributes["dob"].split(
@@ -74,11 +72,20 @@ function SideCard() {
               <p className="mt-3">Description</p>
             </div>
           </div>
-          <div className = "card-footer text-center py-2">
-            <Button className="btn-sm " style={{position:"center", marginLeft:"20%",marginRight:"20%"}} href="/myProfile">View Profile </Button>
+          <div className="card-footer text-center py-2">
+            <Button
+              className="btn-sm "
+              style={{
+                position: "center",
+                marginLeft: "20%",
+                marginRight: "20%",
+              }}
+              href="/myProfile"
+            >
+              View Profile{" "}
+            </Button>
           </div>
         </div>
-        
       </div>
     </div>
   );
